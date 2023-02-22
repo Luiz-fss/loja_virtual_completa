44de1dadd45e3d30075a8a95613f4c8af2e7584d
Luiz-fss <luiz.fss@hotmail.com>
2023-02-21 21:31:01 -0300

    Integracao com Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,31 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async{
   runApp(const MyApp());
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  FirebaseAuth auth = FirebaseAuth.instance;
+
+  var db = FirebaseFirestore.instance;
+  db.collection("users").add({"a":"a"}).then((DocumentReference doc) =>
+      print('DocumentSnapshot added with ID: ${doc.id}'));
+  db.collection("pedidos").doc("idEspecifico").update({"a":"a"});
+  db.collection("colecaoQueQuerLer").doc("idDOcumentoParaLer").get();
+  db.collection("colecao").doc("status").snapshots().listen((event) {
+    //colocar o que você quer fazer quando mudar
+  });
+  db.collection('contatos').doc("key").snapshots().forEach((element) {
+    //criar uma varivael para ir recebendo o contudo do for
+
+    List<DocumentSnapshot> listaDocs =[];
+    listaDocs.add(element);
+  });
+
+
 }
 
 class MyApp extends StatelessWidget {
@@ -12,9 +36,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      home: Container(color: Colors.red,),
     );
   }
 }
