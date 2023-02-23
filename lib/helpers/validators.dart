35a6efd4cad32e4ec87027a33ab9823efd9211a0
Luiz-fss <luiz.fss@hotmail.com>
2023-02-22 22:20:51 -0300

    Validação dos campos de login

---
+++ lib/helpers/validators.dart
@@ -0,0 +1,6 @@
+bool validarEmail(String email){
+  final RegExp regex = RegExp(
+      r"^(([^<>()[\]\\.,;:\s@\']+(\.[^<>()[\]\\.,;:\s@\']+)*)|(\'.+\'))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$");
+
+  return regex.hasMatch(email);
+}
