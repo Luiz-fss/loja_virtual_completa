572a211d2a8c2abeaeff0d25f0d6f19432e6fbd1
Luiz-fss <luiz.fss@hotmail.com>
2023-03-06 20:11:09 -0300

    Marcando item tamanho como selecionado

--- lib/models/produto.dart
+++ lib/models/produto.dart
@@ -1,7 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:loja_virtual_completa/models/tamanho-item.dart';
+import 'package:loja_virtual_completa/telas/produtos/item-tamanho.dart';
 
-class Produto {
+class Produto extends ChangeNotifier{
 
   Produto.fromDocument(DocumentSnapshot documentSnapshot){
     name = documentSnapshot["name"] as String;
@@ -17,6 +19,23 @@ class Produto {
   String? description;
   List<String>? images;
   String? id;
-  List<TamanhoItem>? tamanhos;
+  bool itemSelecionado = false;
+
+
+  marcarItemSelecionado(bool valor){
+    itemSelecionado = valor;
+    notifyListeners();
+  }
+
+  List<TamanhoItem> tamanhos = [];
+
+  TamanhoItem? _tamanhoSelecionado;
+
+  TamanhoItem get selecionarTamanho  => _tamanhoSelecionado!;
+  set selecionarTamanho(TamanhoItem valor){
+    _tamanhoSelecionado = valor;
+    print("notificou");
+    notifyListeners();
+  }
 
 }
