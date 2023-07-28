a28656289d8102ce3ee2477ac184d89915b401ac
Luiz-fss <luiz.fss@hotmail.com>
2023-07-28 18:16:24 -0300

    Ajuste de layout para ter opcções de aumentar ou diminuir a quantidade de estoque

---
+++ lib/componentes-gerais/icon-button-customizado.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class IconButtonCustomizado extends StatelessWidget {
+  const IconButtonCustomizado(
+      {Key? key,
+      required this.iconData,
+      required this.corIcone,
+      required this.onTap})
+      : super(key: key);
+
+  final IconData iconData;
+  final Color corIcone;
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRect(
+      child: Material(
+        color: Colors.transparent,
+        child: InkWell(
+          onTap: onTap,
+          child: Padding(
+            padding: const EdgeInsets.all(4),
+            child: Icon(
+              iconData,
+              color: corIcone,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
